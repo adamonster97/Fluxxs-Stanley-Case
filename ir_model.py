f20cab4f30fc4c9d1512fa905020bc1f99d875e6
Adam Lerner <adamlerner731@gmail.com>
2018-10-04 22:57:58 -0400

    Finished Interest Rate Model
    Completed and working Interest Rate model
    
    Checked to make sure that it function identically to the excel model that was given to us.
    
    This is a stable state of ir_model and ir_param
    
    Co-Authored-By: cadezawacki <cadezawacki@users.noreply.github.com>

--- ir_model.py
+++ ir_model.py
@@ -1,26 +1,57 @@
 import pandas as pd
 import numpy as np
-import ir_param
+import matplotlib.pyplot as plt
+from ir_param import *
 
 NUM_PATHS = 1000
 NUM_PERIODS = 7
 
-def genModel(NUM_PATHS,NUM_PERIODS, s = 0):
+dt = 0.25
+err = 0.001
 
-    factor_1 = np.random.normal(size=(NUM_PATHS,NUM_PERIODS))
-    factor_2 = np.random.normal(size=(NUM_PATHS,NUM_PERIODS))
+def colStr(t,T):
+    return "%.2f_%.2f" % (t, T)
+
+t = 0
+T = 0.5
+
+def genModel(a,phi):
+    param = getParam(a,phi)
+
+    factor_1 = pd.DataFrame(np.random.normal(size=(NUM_PATHS,NUM_PERIODS+1)))
+    #factor_1 = pd.DataFrame([[0,0.0398,  -0.4898, -0.2981, -0.0035, 0.1213,  0.1689,  -0.1743]])
+    factor_1.columns = param.index
+
+    factor_2 = pd.DataFrame(np.random.normal(size=(NUM_PATHS,NUM_PERIODS+1)))
+    #factor_2 = pd.DataFrame([[0,1.8266,  0.1911,  0.1370,  0.1764,  0.2251,  0.7992,  -1.2849]])
+    factor_2.columns = param.index
 
     bondPrices = pd.DataFrame(index=range(NUM_PATHS))
+    YTM = pd.DataFrame(index=range(NUM_PATHS))
 
-    for T in np.arange(0.25,NUM_PATHS/2+0.001,0.25):
-        for t in np.arange(0,T+0.001,0.25):
-            tStr = "%.2f_%.2f" % (t, T)
+    for T in np.arange(dt,NUM_PERIODS*dt+err,dt):
+        for t in np.arange(0,T+err,dt):
+            curStr = colStr(t,T)
+            prevStr = colStr(t-dt, T)
+            print(curStr)
+
             if(t == T):
-                bondPrices[tStr] = 1
+                bondPrices[curStr] = 1
             elif(t == 0):
-                bondPrices[tStr] = np.exp(-1 * param.loc[t,"Zero_YTM"] * t)
+                bondPrices[curStr] = np.exp(-1 * param.loc[(T-0.25),"Zero_YTM"] * T)
             else:
+                bondPrices[curStr] = bondPrices[prevStr]*(
+                        1+YTM[t-dt]*dt +
+                        param.loc[dt:T,"s1"].sum()*dt*np.sqrt(dt) * factor_1.loc[:,t] +
+                        param.loc[dt:T-dt,"s2"].sum()*dt*np.sqrt(dt) * factor_2.loc[:,t] )
 
+            if(t == T-dt):
+                YTM[t] = -np.log(bondPrices[curStr])
 
+    return(bondPrices,YTM)
 
-    YTM = pd.DataFrame()
+(bondPrices,YTM) = genModel(NUM_PATHS,NUM_PERIODS)
+
+plt.plot(YTM.transpose(),color="green")
+plt.plot(YTM.mean(axis=0),linewidth=4,color="blue")
+plt.show()
