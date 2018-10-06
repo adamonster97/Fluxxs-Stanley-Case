e9cd339cdfd45ec362acb09f4971ad487083dfd5
Adam Lerner <adamlerner731@gmail.com>
2018-10-06 16:12:07 -0400

    Modified Param
    - Added ISD as a model parameter
    - Removed Dead Code
    - Updated rounding on YTM
    - Automated Index

--- ir_param.py
+++ ir_param.py
@@ -11,22 +11,21 @@ def s1(a,phi):
     return phi * a
 
 def s2(a,phi):
-    print(a.index - 0.25)
     return a * (np.exp(-2 * (a.index - 0.25)) - 0.5)
-
 
 def getParam(a = 1,phi = 0.05):
     param = pd.DataFrame(
              [0.0520,0.0575,0.0591,0.0601,0.0609,0.0643,0.0667,0.0642], #1 year forward rate
-             [0,0.25,0.5,0.75,1,1.25,1.5,1.75], #index / year
+             np.arange(0,NUM_PERIODS*dt+err,dt), #index / year
              ["F1_t"])
 
-    param["Zero_YTM"] = [0.0520,0.0547500,0.0562,0.0572,0.0579,0.0590,0.0601,0.0606]
+    param["Zero_YTM"] = [0.0520,0.05475,0.0562,0.0572,0.0579,0.0590,0.0601,0.0606]
     #param["F1_t"].expanding().apply(lambda x: ((1+x/4).prod() - 1) / (len(x)/4))
 
     param["a"] = a
 
     param["s1"] = s1(param["a"],phi)
     param["s2"] = s2(param["a"],phi)
+    param["ISD"] =  [0,0.2416,0.2228,0.2016,0.1897,0.1757,0.1609,0.1686]
 
     return param
